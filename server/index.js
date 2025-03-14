92b28082d5b9bb880437af246320168398a85930
RokRask <rokas.raskevic@gmail.com>
2025-03-14 18:40:47 +0200

    commit

--- server/index.js
+++ server/index.js
@@ -22,7 +22,6 @@ mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-
 .then(() => {
   console.log('Prisijungta prie MongoDB duomenų bazės');
 })
